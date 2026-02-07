402b18df56c038031100b82f25bf479125eab010
tescu <tescu@tutamail.com>
2026-02-07 09:43:29 +0200

    add Korean

---
+++ reverse-tsv.lua
@@ -0,0 +1,30 @@
+#!/usr/bin/env lua
+-- Reverse word order in a TSV dict file
+-- Read and parse the tsv file
+local function readtsv(filename)
+	local data = {}
+	local i=1
+	
+	-- Open the file for reading
+	local file = io.open(filename, "r")
+	if not file then
+		error("Could not open "..filename)
+	end
+	
+	-- Read each line in the file
+	for line in file:lines() do
+
+		local fields = {}
+		for field in line:gmatch("([^\t]+)") do
+			table.insert(fields, field)
+		end
+		print(fields[1]..'\t'..fields[2])
+		i=i+1
+	end
+	
+	-- Close the file
+	file:close()
+	return fields
+end
+--readtsv('res/en-ro.tsv')
+readtsv(arg[1])
